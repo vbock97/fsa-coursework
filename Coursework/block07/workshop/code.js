660bbf31d07c154ffb505e72540a6ce4c20b9fea
Victoria Bock <thenameisjude@Victorias-Mini.home>
2024-10-01 19:06:33 -0700

    block 7 workshop

---
+++ Coursework/block07/workshop/code.js
@@ -0,0 +1,18 @@
+/**
+ * 1. Declare a string variable and assign it to the value 
+ * "You have received this message because you have been chosen to open an important vault"
+ * 2. Declasre and assign 3 variables, one for each number in the commbination
+ * 3. Code1 will be using addition
+ * 4. Code2 will be using subtraction
+ * 5. Code3 will be using multiplication
+ * 6. Display a string and the codes to the user
+ */
+
+
+const userMessage = "You have received this message because you have been chosen to open an important vault"
+
+let code1 = 1 + 1
+let code2 = 10 - 5
+let code3 = 7 * 3
+
+alert(userMessage + "\n" + code1 +  code2 + code3)
