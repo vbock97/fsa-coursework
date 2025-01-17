a1fb6ddf7401a5486f38c18088fcc50b99f2eea6
Victoria Bock <thenameisjude@Victorias-Mini.home>
2025-01-16 18:52:19 -0800

    updated Unit3 block26

--- Coursework/Unit3/block26/workshop/contact-list/src/components/SelectedContact.jsx
+++ Coursework/Unit3/block26/workshop/contact-list/src/components/SelectedContact.jsx
@@ -12,7 +12,7 @@ export default function SelectedContact({
       if (!selectedContactId) return;
       try {
         const response = await fetch(
-          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/"
+          `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`
         );
         const result = await response.json();
         setContact(result);
@@ -29,13 +29,15 @@ export default function SelectedContact({
   }
 
   return (
-    <div>
-      <h2>Contact Details</h2>
-      <p>Name: {contact.name}</p>
-      <p>Email: {contact.email}</p>
-      <p>Phone: {contact.phone}</p>
-      <p>Website: {contact.website}</p>
-      <button onClick={() => setSelectedContactId(null)}>Back to List</button>
+    contact && (
+      <div>
+        <h2>Contact Details</h2>
+        <p>Name: {contact.name}</p>
+        <p>Email: {contact.email}</p>
+        <p>Phone: {contact.phone}</p>
+        <p>Website: {contact.website}</p>
+        <button onClick={() => setSelectedContactId(null)}>Back to List</button>
       </div>
+    )
   );
 }
